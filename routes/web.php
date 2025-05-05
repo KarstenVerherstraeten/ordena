68baea284edda61d887f7b7ab985dab7bd836c66
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-05 20:21:08 +0200

    Refactor: cleaned up the routing system

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+use App\Http\Controllers\PostController;
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -18,18 +21,15 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/forum', function () {
-    return Inertia::render('Forum', [
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('forum');
+Route::get('/forum', [PostController::class, 'index'])->name('forum');
 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/forum', [PostController::class, 'store'])->middleware(['auth'])->name('forum.store');
 });
 
 require __DIR__.'/auth.php';
