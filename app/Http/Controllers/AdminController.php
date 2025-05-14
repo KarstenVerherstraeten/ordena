04b3479558b4fe182f1048403a7f7fa63a07f23a
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-14 11:22:29 +0200

    Chore: Replaced the admin panel pages into a folder

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 
     public function index()
     {
-        return Inertia::render('AdminPanel', [
+        return Inertia::render('AdminPanel/AdminPanel', [
 
 
         ]);
--- resources/js/Pages/AdminPanel/AdminPanel.jsx
+++ resources/js/Pages/AdminPanel/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import AdminLayout from '@/Layouts/adminLayout';
+import AdminLayout from '@/Layouts/AdminLayout.jsx';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard() {
@@ -18,6 +18,15 @@ export default function Dashboard() {
                         <div className="p-6 text-gray-900">
                             You're logged in!
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900">
+                        <h2 className="text-lg font-bold my-4">Admin Dashboard</h2>
+                        <p>Welcome to the admin panel. Here you can manage users, posts, and more.</p>
                     </div>
                 </div>
             </div>
