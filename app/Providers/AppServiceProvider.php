300c20a375eebc5d271f895964bb43830061ca05
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-14 10:18:56 +0200

    Feat: share the detail data for global useage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Inertia::share([
+            'auth' => function () {
+                $user = Auth::user();
+                if ($user) {
+                    $user->load('detail'); // Load the relation
+                }
+
+                return [
+                    'user' => $user,
+                ];
+            },
+        ]);
+
         Vite::prefetch(concurrency: 3);
     }
 }
