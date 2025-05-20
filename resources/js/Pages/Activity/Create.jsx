a90eca8fde98dd518a246ebc2319176afbae32f4
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-20 16:24:26 +0200

    Feat: Document cleanup + added if 0 add "gratis"

--- resources/js/Pages/Activity/Create.jsx
+++ resources/js/Pages/Activity/Create.jsx
@@ -4,10 +4,9 @@ import PrimaryButton from "@/Components/PrimaryButton.jsx";
 import {useForm} from "@inertiajs/react";
 
 
-
 export default function ActivitiesCreate() {
 
-    const { data, setData, post, processing, errors } = useForm({
+    const {data, setData, post, processing, errors} = useForm({
         title: '',
         description: '',
         startDate: '',
@@ -23,9 +22,9 @@ export default function ActivitiesCreate() {
         post(route('activities.store'));
     }
 
-    return(
+    return (
         <div className="py-12">
-            <Head title="Activiteiten" />
+            <Head title="Activiteiten"/>
             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div className="p-6 text-gray-900">
@@ -42,95 +41,96 @@ export default function ActivitiesCreate() {
                             <div className="mb-4">
                                 <label htmlFor="title" className="block text-gray-700">Naam</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="text"
-                                id="title"
-                                name="title"
-                                value={data.title}
-                                onChange={(e) => setData('title', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                    value={data.title}
+                                    onChange={(e) => setData('title', e.target.value)}
+                                    required
                                 />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="description" className="block text-gray-700">Beschrijving</label>
                                 <textarea
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                id="description"
-                                name="description"
-                                value={data.description}
-                                onChange={(e) => setData('description', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    id="description"
+                                    name="description"
+                                    value={data.description}
+                                    onChange={(e) => setData('description', e.target.value)}
+                                    required
                                 />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="StartDate" className="block text-gray-700">Startdatum</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="datetime-local"
-                                id="StartDate"
-                                name="StartDate"
-                                value={data.startDate}
-                                onChange={(e) => setData('startDate', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="datetime-local"
+                                    id="StartDate"
+                                    name="StartDate"
+                                    value={data.startDate}
+                                    onChange={(e) => setData('startDate', e.target.value)}
+                                    required
                                 />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="EndDate" className="block text-gray-700">Einddatum</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="datetime-local"
-                                id="EndDate"
-                                name="EndDate"
-                                value={data.endDate}
-                                onChange={(e) => setData('endDate', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="datetime-local"
+                                    id="EndDate"
+                                    name="EndDate"
+                                    value={data.endDate}
+                                    onChange={(e) => setData('endDate', e.target.value)}
+                                    required
                                 />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="Price" className="block text-gray-700">Prijs</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="number"
-                                id="Price"
-                                name="Price"
-                                value={data.price}
-                                onChange={(e) => setData('price', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="number"
+                                    id="Price"
+                                    name="Price"
+                                    value={data.price}
+                                    onChange={(e) => setData('price', e.target.value)}
                                 />
+                                <p> Vul hier 0 in om je
+                                    activiteit gratis aan te bieden</p>
+
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="Location" className="block text-gray-700">Locatie</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="text"
-                                id="Location"
-                                name="Location"
-                                value={data.location}
-                                onChange={(e) => setData('location', e.target.value)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="text"
+                                    id="Location"
+                                    name="Location"
+                                    value={data.location}
+                                    onChange={(e) => setData('location', e.target.value)}
+                                    required
                                 />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="featured_image" className="block text-gray-700">Hoofdafbeelding</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="file"
-                                id="featured_image"
-                                name="featured_image"
-                                onChange={(e) => setData('featured_image', e.target.files[0])}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="file"
+                                    id="featured_image"
+                                    name="featured_image"
+                                    onChange={(e) => setData('featured_image', e.target.files[0])}
                                 />
                             </div>
                             <div className="mb-4">
-                                <label htmlFor="activity_images" className="block text-gray-700">Activiteit Afbeeldingen</label>
+                                <label htmlFor="activity_images" className="block text-gray-700">Activiteit
+                                    Afbeeldingen</label>
                                 <input
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                                type="file"
-                                multiple
-                                id="activity_images"
-                                name="activity_images[]"
-                                onChange={(e) => setData('activity_images', e.target.files)}
-                                required
+                                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                    type="file"
+                                    multiple
+                                    id="activity_images"
+                                    name="activity_images[]"
+                                    onChange={(e) => setData('activity_images', e.target.files)}
                                 />
                             </div>
                             {errors && (
