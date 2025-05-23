b5d49c305ccb423dc572de5bd8570ce67839d753
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-23 13:15:22 +0200

    Feat: made migrations for the organisations

---
+++ database/migrations/2025_05_23_120502_pending_organisation_requests.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pending_organisation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('organisation_name');
+            $table->string('organisation_address');
+            $table->string('BTW_number'); // VAT number
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pending_organisation_requests');
+    }
+};
