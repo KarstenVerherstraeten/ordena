07d8cde0f9e318d3a9e8d0cddc848fac6bd69672
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2025-05-27 17:07:18 +0200

    Fix: Logo classes
    The logo took over the styling of the dash paths due to same class

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,38 +1,38 @@
 export default function ApplicationLogo(props) {
     return (
-        <svg id="Laag_1" data-name="Laag 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 291.91 109.68"
+        <svg id="logo" data-name="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 291.91 109.68"
              style={{width: '100%', height: 'auto'}} {...props}>
             <defs>
                 <style>
-                    {`.cls-1 {
+                    {`.logo-text-spacing {
                         letter-spacing: 0em;
                     }
-                    .cls-2 {
+                    .logo-shape-1 {
                         fill: rgba(150, 119, 180, .5);
                     }
-                    .cls-3 {
+                    .logo-shape-2 {
                         fill: #c4e2d3;
                         opacity: .5;
                     }
-                    .cls-4 {
+                    .logo-text {
                         fill: #9677b3;
                         font-family: ApparatSemiCond-Heavy, ApparatSemiCond;
                         font-size: 60px;
                         font-weight: 800;
                     }
-                    .cls-5 {
+                    .logo-letter-spacing {
                         letter-spacing: -.01em;
                     }`}
                 </style>
             </defs>
-            <path className="cls-2"
+            <path className="logo-shape-1"
                   d="M0,55.06c.09-14.19,8.75-28.71,22.29-32.96,4.85-1.52,10.41-1.64,14.78.96,8.82,5.23,9.03,18.25,16.05,25.74,4.84,5.16,12.17,6.93,18.81,9.39,6.64,2.46,13.25,6.78,15.1,13.61,6.6,24.34-20.8,39.47-40.76,37.77-11.39-.97-21.37-8.38-29.3-16.62C7.13,82.71-.09,69.24,0,55.06Z"/>
-            <path className="cls-3"
+            <path className="logo-shape-2"
                   d="M290.85,13.71c2.31,4.14.55,8.75-2.43,11.59-2.98,2.84-7.01,4.54-10.2,7.17-6.34,5.23-8.79,13.57-11.25,21.47-2.46,7.9-5.46,16.12-12.47,20.59-7.01,4.47-19.29,3.24-24.67-4.66-4.55-6.69-2.32-14.47-.11-21.09,2.21-6.63,3.93-14.75-1.26-21.07-2.25-2.75-5.52-4.69-7.72-7.47-4.56-5.75-3.04-13.6,1.76-17.61.03-.03.07-.06.1-.09,4.37-3.58,11.33-3.31,16.87.51,7.66,5.28,17.1,8.02,25.49,6.88,4.5-.61,8.68-2.22,13.32-2.31,4.64-.09,10.21,1.88,12.56,6.09Z"/>
-            <text className="cls-4" transform="translate(43.44 73.27)">
+            <text className="logo-text" transform="translate(43.44 73.27)">
                 <tspan x="0" y="0">O</tspan>
-                <tspan className="cls-5" x="48.42" y="0">r</tspan>
-                <tspan className="cls-1" x="71.4" y="0">dena</tspan>
+                <tspan className="logo-letter-spacing" x="48.42" y="0">r</tspan>
+                <tspan className="logo-text-spacing" x="71.4" y="0">dena</tspan>
             </text>
         </svg>
     );
